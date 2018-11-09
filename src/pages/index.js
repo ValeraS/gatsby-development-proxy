37e25d6e530a02502be98730c85b25416548a9f6
Valeriy S <ValeraS@users.noreply.github.com>
2018-11-09 20:13:32 +0300

    add request api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import axios from 'axios'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: '',
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    try {
+      const response = await axios.get('api/data');
+      this.setState({ data: response.data });
+    } catch (err) {
+      this.setState({ data: 'Error!!!' });
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+          <Image />
+        </div>
+        <p>Data: {this.state.data}</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
